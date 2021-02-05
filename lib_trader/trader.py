99d22eb182cfbed4efdfbfc29d2805a9bc06b585
Justin Furuness <jfuruness@gmail.com>
2021-02-05 02:46:22 -0500

    functional

--- lib_trader/trader.py
+++ lib_trader/trader.py
@@ -11,6 +11,7 @@ __email__ = "jfuruness@gmail.com"
 __status__ = "Development"
 
 from datetime import datetime
+import time
 import re
 
 import email
@@ -34,9 +35,15 @@ class Trader:
         self.wb_start_time = datetime.now()
         _email, trade_token, password = Config().webull_creds()
         self.wb.get_mfa(_email)
+        print("sleeping 15 secs")
+        time.sleep(15)
         mfa = self.get_webull_verification_code()
+        print("got mfa", mfa)
+        time.sleep(1)
         self.trade_token = trade_token
         result = self.wb.login(_email, password, "JMF", mfa)
+        print("got result")
+        time.sleep(1)
         for err in ["Incorrect", "Wrong format"]:
             assert err not in str(result)
         utils.write_to_stdout(result)
@@ -93,17 +100,19 @@ class Trader:
                 Actions.ADD: self.add}
 
     def buy(self, alert):
-        self.wb.place_order(stock=alert.ticker,
-                            action="BUY",
-                            orderType="MKT",
-                            quant=alert.shares,
-                            enforce="DAY")
+        print(self.wb.place_order(stock=alert.ticker,
+                                  action="BUY",
+                                  orderType="MKT",
+                                  quant=alert.shares,
+                                  enforce="DAY"))
     def sell(self, alert):
-        self.wb.place_order(stock=alert.ticker,
-                            action="SELL",
-                            orderType="MKT",
-                            quant=alert.shares,
-                            enforce="DAY")
+        print(alert.shares)
+        print(alert.ticker)
+        print(self.wb.place_order(stock=alert.ticker,
+                                  action="SELL",
+                                  orderType="MKT",
+                                  quant=alert.shares,
+                                  enforce="DAY"))
     def short(self, alert):
         utils.write_to_stdout("Placeholder for short")
     def cover(self, alert):
